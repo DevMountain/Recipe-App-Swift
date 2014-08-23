5a71abfb4924f5eeaeb925b5132dc1aa1abfe008
Joshua Howland <development@jkhowland.com>
2014-08-23 09:45:59 -0600

    Inital commit

---
+++ Recipe-App-Swift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Recipe-App-Swift
+//
+//  Created by Joshua Howland on 8/22/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+                            
+    var window: UIWindow?
+
+
+    func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        // Override point for customization after application launch.
+        self.window!.backgroundColor = UIColor.whiteColor()
+        self.window!.makeKeyAndVisible()
+        return true
+    }
+
+}
+
