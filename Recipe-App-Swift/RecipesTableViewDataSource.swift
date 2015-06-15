3511d5a7a496a05ed2e2512ff5b809c4d64046f4
Andrew Madsen <andrew@openreelsoftware.com>
2015-06-14 19:04:11 -0600

    Updated to latest Swift (Xcode 6.3 / Swift 1.2).

--- Recipe-App-Swift/RecipesTableViewDataSource.swift
+++ Recipe-App-Swift/RecipesTableViewDataSource.swift
@@ -10,20 +10,20 @@ import UIKit
 
 class RecipesTableViewDataSource: NSObject, UITableViewDataSource {
  
-    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return RARecipes.count()
     }
-    
-    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
-        
+
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		
         var cell :UITableViewCell? = tableView.dequeueReusableCellWithIdentifier("cell") as? UITableViewCell
         if cell == nil {
             cell = UITableViewCell()
         }
         
-        cell?.textLabel.text = RARecipes.titleAtIndex(indexPath.row)
+        cell!.textLabel!.text = RARecipes.titleAtIndex(indexPath.row)
 
-        return cell
+        return cell!
     }
     
 }
